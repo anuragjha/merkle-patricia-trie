e1d74507f1e9cb45953b4596195cf39c4b957c68
Anurag Jha <anuragjha025@gmail.com>
2019-02-14 02:31:33 -0800

    refactor and testcases

--- p1/insertHelper.go
+++ p1/insertHelper.go
@@ -27,6 +27,8 @@ func (mpt *MerklePatriciaTrie) InsertRecursive(node Node, leftPath []uint8, newV
 		return hash
 	} else if len(leftPath) == 0 {
 		fmt.Println("LEFT PATH IS ZERO TODO...")
+		//lib
+		fmt.Println("currNode", currNode)
 		if currNode.node_type == 1 {
 			currNode.branch_value[16] = newValue
 			hash := currNode.hash_node()
@@ -190,7 +192,14 @@ func (mpt *MerklePatriciaTrie) InsertRecursive(node Node, leftPath []uint8, newV
 					//check the lenght of the leaf it 1 then create leaf store empty value
 					//lenght of leaf is 0 .... i think it is the same
 					//lenght of leaf is > 1 ....store in leaf
+					//lib
 					currNode.flag_value.value = mpt.InsertRecursive(mpt.db[currNode.flag_value.value], nil, newValue, nil)
+					hash := currNode.hash_node()
+					delete(mpt.db, old_hash)
+					mpt.db[hash] = currNode
+					fmt.Println("Returning hash")
+					return hash
+					//lib
 				} else if check_if_common_path_exists(leftPath, decodedHexArray) {
 					counter := 0
 					for i := 0; i < len(decodedHexArray); i++ {
@@ -211,6 +220,7 @@ func (mpt *MerklePatriciaTrie) InsertRecursive(node Node, leftPath []uint8, newV
 					// 		break
 					// 	}
 					//}
+
 					fmt.Println("Counter:", counter)
 					common_path2 := leftPath[:counter]
 					leftPath = leftPath[counter:]
@@ -219,45 +229,54 @@ func (mpt *MerklePatriciaTrie) InsertRecursive(node Node, leftPath []uint8, newV
 					fmt.Println("common_path2:", common_path2)
 					fmt.Println("leftPath:", leftPath)
 					fmt.Println("leftNibble:", leftNibble)
-					nodeBranch := Node{}
-					nodeBranch.node_type = 1
-					if len(leftPath) > 0 {
-						//create Branch
-						//left path create branch and leaf
-						leftPath = append(leftPath, 16)
+					if len(leftNibble) > 0 { // lib
+						nodeBranch := Node{}
+						nodeBranch.node_type = 1
+						if len(leftPath) > 0 {
+							//create Branch
+							//left path create branch and leaf
+							leftPath = append(leftPath, 16)
 
-						nodeLeaf := Node{}
-						nodeLeaf.node_type = 2
-						nodeLeaf.flag_value.encoded_prefix = compact_encode(leftPath[1:])
-						nodeLeaf.flag_value.value = newValue
-						fmt.Println("Inserting in branch at index:", leftPath[0])
-						nodeBranch.branch_value[leftPath[0]] = mpt.InsertRecursive(nodeLeaf, nil, "", nil)
-					} else {
-						nodeBranch.branch_value[16] = newValue
-					}
-					//check the left nibble size
-					if len(leftNibble) == 0 {
+							nodeLeaf := Node{}
+							nodeLeaf.node_type = 2
+							nodeLeaf.flag_value.encoded_prefix = compact_encode(leftPath[1:])
+							nodeLeaf.flag_value.value = newValue
+							fmt.Println("Inserting in branch at index:", leftPath[0])
+							nodeBranch.branch_value[leftPath[0]] = mpt.InsertRecursive(nodeLeaf, nil, "", nil)
+						} else {
+							nodeBranch.branch_value[16] = newValue
+						}
+						//check the left nibble size
+						if len(leftNibble) == 0 {
+							currNode.flag_value.value = mpt.InsertRecursive(mpt.db[currNode.flag_value.value], leftPath, newValue, nil)
+							hashExt := currNode.hash_node()
+							delete(mpt.db, old_hash)
+							mpt.db[hashExt] = currNode
+							return hashExt
+						} else if len(leftNibble) == 1 {
+							nodeBranch.branch_value[leftNibble[0]] = currNode.flag_value.value
+							currNode.flag_value.value = mpt.InsertRecursive(nodeBranch, nil, "", nil)
+							hashExt := currNode.hash_node()
+							delete(mpt.db, old_hash)
+							mpt.db[hashExt] = currNode
+							return hashExt
+						} else if len(leftNibble) > 1 {
+							//create an extension and store the value of that extension in branch => branch hash store in currNode
+							nodeExtension := Node{}
+							nodeExtension.node_type = 2
+							nodeExtension.flag_value.encoded_prefix = compact_encode(leftNibble[1:])
+							nodeExtension.flag_value.value = currNode.flag_value.value
+							fmt.Println("Inserting in branch at index:", leftNibble[0])
+							nodeBranch.branch_value[leftNibble[0]] = mpt.InsertRecursive(nodeExtension, nil, "", nil)
+							currNode.flag_value.value = mpt.InsertRecursive(nodeBranch, nil, "", nil)
+							hashExt := currNode.hash_node()
+							delete(mpt.db, old_hash)
+							mpt.db[hashExt] = currNode
+							return hashExt
+						}
+					} else { //lib
+						fmt.Println("Proceeding ahead from extension") //lib
 						currNode.flag_value.value = mpt.InsertRecursive(mpt.db[currNode.flag_value.value], leftPath, newValue, nil)
-						hashExt := currNode.hash_node()
-						delete(mpt.db, old_hash)
-						mpt.db[hashExt] = currNode
-						return hashExt
-					} else if len(leftNibble) == 1 {
-						nodeBranch.branch_value[leftNibble[0]] = currNode.flag_value.value
-						currNode.flag_value.value = mpt.InsertRecursive(nodeBranch, nil, "", nil)
-						hashExt := currNode.hash_node()
-						delete(mpt.db, old_hash)
-						mpt.db[hashExt] = currNode
-						return hashExt
-					} else if len(leftNibble) > 1 {
-						//create an extension and store the value of that extension in branch => branch hash store in currNode
-						nodeExtension := Node{}
-						nodeExtension.node_type = 2
-						nodeExtension.flag_value.encoded_prefix = compact_encode(leftNibble[1:])
-						nodeExtension.flag_value.value = currNode.flag_value.value
-						fmt.Println("Inserting in branch at index:", leftNibble[0])
-						nodeBranch.branch_value[leftNibble[0]] = mpt.InsertRecursive(nodeExtension, nil, "", nil)
-						currNode.flag_value.value = mpt.InsertRecursive(nodeBranch, nil, "", nil)
 						hashExt := currNode.hash_node()
 						delete(mpt.db, old_hash)
 						mpt.db[hashExt] = currNode
